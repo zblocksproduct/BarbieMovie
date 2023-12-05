601d2519ae73d24ebc998ded68f088b1d646641c
zblocksproduct <151188302+zblocksproduct@users.noreply.github.com>
2023-12-05 13:54:57 +0000

    Update index.js

--- src/pages/barbie/index.js
+++ src/pages/barbie/index.js
@@ -75,7 +75,7 @@ export default function Barbei() {
             <div
               onClick={() => {
                 window.location.href =
-              "https://wallettest.zblocks.io?gift=38f2e57d0e2067e1bc75f01cef47&hint=microsoft";
+              "https://wallettest.zblocks.io?gift=5a9d131b373aa6db607e6ca92183&hint=microsoft";
               }}
               className="max-w-[360px] cursor-pointer w-[100%] h-[74px] bg-[#E93FA6] rounded-full text-white flex justify-center items-center text-[22px] font-medium"
             >
@@ -85,7 +85,7 @@ export default function Barbei() {
             <div
               onClick={() => {
                 window.location.href =
-              "https://wallettest.zblocks.io?gift=38f2e57d0e2067e1bc75f01cef47&hint=google";
+              "https://wallettest.zblocks.io?gift=5a9d131b373aa6db607e6ca92183&hint=google";
               }}
               className="max-w-[360px] cursor-pointer w-[100%] sm:ml-[30px] sm:mt-0 mt-[20px] h-[74px] bg-[#E93FA6] rounded-full text-white flex justify-center items-center text-[22px] font-medium"
             >
